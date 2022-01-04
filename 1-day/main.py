cf965f0e36a0aee86f27999694f43beddee8ff30
= <vielendanke1991@gmail.com>
2022-01-04 16:47:07 +0600

    day 1 done

---
+++ 1-day/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator!")
+city = input("What is your city?\n")
+pet_name = input("What is the name of your pet?\n")
+print("Your band name could be " + city + " " + pet_name)
