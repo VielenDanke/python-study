b396ad3dc16654a6ace76dba15ba1b85f15bfa4f
= <vielendanke1991@gmail.com>
2022-01-10 11:28:35 +0600

    second day

--- 3_day/upgraded_bmi_calculator.py
+++ 3_day/upgraded_bmi_calculator.py
@@ -4,12 +4,12 @@ weight = float(input("enter your weight in kg: "))
 bmi = round(weight / height ** 2)
 
 if bmi < 18.5:
-    print(f"underweight. bmi {bmi}")
+    print(f"Your BMI is {bmi}, you are underweight.")
 elif bmi < 25:
-    print(f"normal weight. bmi {bmi}")
+    print(f"Your BMI is {bmi}, you have a normal weight.")
 elif bmi < 30:
-    print(f"overweight. bmi {bmi}")
+    print(f"Your BMI is {bmi}, you are slightly overweight.")
 elif bmi < 35:
-    print(f"obese. bmi {bmi}")
+    print(f"Your BMI is {bmi}, you are obese.")
 else:
-    print(f"clinically obese. bmi {bmi}")
+    print(f"Your BMI is {bmi}, you are clinically obese.")
