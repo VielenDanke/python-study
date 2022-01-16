80ca87463dbbbce5d725d9bbff4e86e4d9f3da6f
= <vielendanke1991@gmail.com>
2022-01-16 22:21:40 +0600

    day 10 - functions with return. Calculator project

---
+++ 10_functions_with_output/calculator/calculator.py
@@ -0,0 +1,57 @@
+from art import logo
+
+
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    if n2 == 0:
+        return "Cannot divide to 0"
+    return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+print(logo)
+
+
+def calculator():
+    is_continue = True
+
+    num1 = float(input("Insert the number?: "))
+
+    for k in operations:
+        print(k)
+
+    while is_continue:
+        user_operation_symbol = input("Pick an operation?: ")
+        num2 = float(input("What is the next number?: "))
+
+        result = operations[user_operation_symbol](num1, num2)
+        print(f"{num1} {user_operation_symbol} {num2} = {result}")
+        num1 = result
+        user_answer = input("Do you want to continue? Type 'y' or 'n' to start a new operation. To stop the program - "
+                            "type any symbol instead ")
+        if user_answer == "n":
+            is_continue = False
+            calculator()
+        elif user_answer != "y":
+            print("Unknown answer. Break program")
+            return
+
+
+calculator()
